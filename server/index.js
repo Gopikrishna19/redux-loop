ac99f73f0e1385df3aebdeb3a87c7fccdcd722f9
Gopi <sandalblack19@gmail.com>
2016-01-30 23:26:33 -0600

    index page for the app with simple template

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function () {
+
+  return (
+    <html>
+      <head>
+        <title>Redux Loop</title>
+        <link rel="stylesheet" type="text/css" href="index.css"/>
+      </head>
+      <body>
+        <div id="app"></div>
+        <script src="index.js"></script>
+      </body>
+    </html>
+  );
+
+}
