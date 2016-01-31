667f11c97bff9ffa4b2c9446f3e223a23d20fb11
Gopi <sandalblack19@gmail.com>
2016-01-31 13:15:48 -0600

    Added logic for totaling on item toggle
    still has some bugs -> have to calculate based on sourceType only

--- src/reducers/items-meta-reducer.js
+++ src/reducers/items-meta-reducer.js
@@ -1,13 +1,70 @@
-function reduce(state) {
-  return state;
+function sanitize(number) {
+  return parseFloat(number.toFixed(2));
 }
 
-export default function (state, action) {
+function getAdjustedPayAmount(item, meta) {
+  return sanitize(meta.payAmount + ((item.selected ? 1 : -1) * item.payAmount));
+}
+
+function getTotalAmountOwed(items) {
+  return sanitize(items.reduce((sum, item) => item.amountOwed + sum, 0));
+}
+
+function getTotalPayAmount(items) {
+  return sanitize(items.reduce((sum, item) => item.selected ? item.amountOwed + sum : sum, 0));
+}
+
+function isEveryItemSelected(items) {
+  return !items.some(item => !item.selected);
+}
+
+function setMetaData(items) {
+
+  return {
+    allSelected: isEveryItemSelected(items),
+    amountOwed: getTotalAmountOwed(items),
+    payAmount: getTotalPayAmount(items)
+  }
+
+}
+
+function toggleAll(items, meta, action) {
+
+  return Object.assign({}, meta, {
+    allSelected: action.selection,
+    payAmount: getTotalPayAmount(items)
+  });
+
+}
+
+function toggleItem(items, meta, action) {
+
+  return Object.assign({}, meta, {
+    allSelected: action.selection && isEveryItemSelected(items),
+    payAmount: getAdjustedPayAmount(items[action.index], meta)
+  });
+
+}
+
+const defaultReducer = items => items;
+const reducers = {
+  SET_DATA: setMetaData,
+  TOGGLE_ALL: toggleAll,
+  TOGGLE_ITEM: toggleItem
+};
+
+function reduce(items, meta, action) {
+  return (reducers[action.type] || defaultReducer)(items, meta, action);
+}
+
+export default function (state = {}, action) {
+
+  state.meta = state.meta || {};
 
   return {
     meta: {
-      parts: reduce(state.parts, action),
-      miscItems: reduce(state.miscItems, action)
+      parts: reduce(state.parts, state.meta.parts, action),
+      miscItems: reduce(state.miscItems, state.meta.miscItems, action)
     }
   };
 
--- src/reducers/totals-reducer.js
+++ src/reducers/totals-reducer.js
@@ -1,11 +1,31 @@
-function reduce(state) {
-  return state;
+function sanitize(number) {
+  return parseFloat(number.toFixed(2));
+}
+
+function setTotals(meta) {
+
+  return {
+    amountOwed: sanitize(meta.parts.amountOwed + meta.miscItems.amountOwed),
+    payAmount: sanitize(meta.parts.payAmount + meta.miscItems.payAmount)
+  };
+
+}
+
+const defaultReducer = meta => meta;
+const reducers = {
+  SET_DATA: setTotals,
+  TOGGLE_ALL: setTotals,
+  TOGGLE_ITEM: setTotals
+};
+
+function reduce(meta, action) {
+  return (reducers[action.type] || defaultReducer)(meta, action);
 }
 
 export default function (state, action) {
   return {
     meta: Object.assign({}, state.meta, {
-      grandTotal: reduce(state, action)
+      grandTotal: reduce(state.meta, action)
     })
   }
 }
